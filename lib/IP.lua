57d732652837e6bcff5c2f3ff3cd0694a1562dd6
Creator <wassil.janssen@gmail.com>
2016-02-13 14:38:36 +0200

    Working on Utils and IP libs.

--- lib/IP.lua
+++ lib/IP.lua
@@ -30,30 +30,29 @@ function IP.new(input)
 end
 
 IP.reserved = {
-	IP.new("0.0.0.0/8"), 		--Current network (only valid as source address) 	RFC 6890
-	IP.new("10.0.0.0/8"), 		--Private network 	RFC 1918
-	IP.new("100.64.0.0/10"), 	--Shared Address Space 	RFC 6598
-	IP.new("127.0.0.0/8"), 	--Loopback 	RFC 6890
-	IP.new("169.254.0.0/16"), 	--Link-local 	RFC 3927
-	IP.new("172.16.0.0/12"), 	--Private network 	RFC 1918
-	IP.new("192.0.0.0/24"), 	--IETF Protocol Assignments 	RFC 6890
-	IP.new("192.0.2.0/24"), 	--TEST-NET-1, documentation and examples 	RFC 5737
-	IP.new("192.88.99.0/24"), 	--IPv6 to IPv4 relay 	RFC 3068
-	IP.new("192.168.0.0/16"), 	--Private network 	RFC 1918
-	IP.new("198.18.0.0/15"), 	--Network benchmark tests 	RFC 2544
-	IP.new("198.51.100.0/24"), --TEST-NET-2, documentation and examples 	RFC 5737
-	IP.new("203.0.113.0/24"), 	--TEST-NET-3, documentation and examples 	RFC 5737
-	IP.new("224.0.0.0/4"), 	--IP multicast (former Class D network) 	RFC 5771
-	IP.new("240.0.0.0/4"), 	--Reserved (former Class E network) 	RFC 1700
-	IP.new("255.255.255.255"), --Broadcast	RFC 919
-}
-
-IP.ids = {
-	
+	[0] = IP.new("0.0.0.0/8"), 			--0x0		Current network (only valid as source address) 	RFC 6890
+	[1] = IP.new("10.0.0.0/8"), 		--0x1		Private network 	RFC 1918
+	[2] = IP.new("100.64.0.0/10"), 		--0x2		Shared Address Space 	RFC 6598
+	[3] = IP.new("127.0.0.0/8"), 		--0x3		Loopback 	RFC 6890
+	[4] = IP.new("169.254.0.0/16"), 	--0x4		Link-local 	RFC 3927
+	[5] = IP.new("172.16.0.0/12"), 		--0x5		Private network 	RFC 1918
+	[6] = IP.new("192.0.0.0/24"), 		--0x6		IETF Protocol Assignments 	RFC 6890
+	[7] = IP.new("192.0.2.0/24"), 		--0x7		TEST-NET-1, documentation and examples 	RFC 5737
+	[8] = IP.new("192.88.99.0/24"), 	--0x8		IPv6 to IPv4 relay 	RFC 3068
+	[9] = IP.new("192.168.0.0/16"), 	--0x9		Private network 	RFC 1918
+	[10] = IP.new("198.18.0.0/15"), 	--0xa		Network benchmark tests 	RFC 2544
+	[11] = IP.new("198.51.100.0/24"), 	--0xb		TEST-NET-2, documentation and examples 	RFC 5737
+	[12] = IP.new("203.0.113.0/24"), 	--0xc		TEST-NET-3, documentation and examples 	RFC 5737
+	[13] = IP.new("224.0.0.0/4"), 		--0xd		IP multicast (former Class D network) 	RFC 5771
+	[14] = IP.new("240.0.0.0/4"), 		--0xe		Reserved (former Class E network) 	RFC 1700
+	[15] = IP.new("255.255.255.255/32"), 	--0xf		Broadcast	RFC 919
 }
 
 function IP.isReserved(address)
-	if 
+	for i=0,15 do
+		print(IP.reserved[i].network)
+		print(IP.reserved[i].length)
+	end
 end
 
 return IP

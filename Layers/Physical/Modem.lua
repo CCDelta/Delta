97fc93ec489d972951ea16b5639dbc199d99500c
Creator <wassil.janssen@gmail.com>
2016-02-13 23:52:30 +0200

    Simplified MAC generation && built 16w5c

--- Layers/Physical/Modem.lua
+++ Layers/Physical/Modem.lua
@@ -4,6 +4,10 @@
 
 local Delta = ...
 local SHA = Delta.lib.SHA
+local BigNum = Delta.lib.BigNum
+local toBase = Delta.lib.Utils.toBase
+local bxor = bit.bxor
+local largest = BigNum.new("281474976710597")
 
 local sides = {
 	top 	= 0,
@@ -25,5 +29,6 @@ return function(side)
 	local _2 = tonumber("0x"..s:sub(17,32))
 	local _3 = tonumber("0x"..s:sub(33,48))
 	local _4 = tonumber("0x"..s:sub(49,64))
-	local _64bit = b
+	local _64bit = tostring(bxor(bxor(_1,_2),bxor(_3,_4)))
+	print(_64bit)
 end
--- lib/Utils.lua
+++ lib/Utils.lua
@@ -12,12 +12,14 @@ function Utils.toBase(val,base,fill)
 	local base, k = base or 10, "0123456789ABCDEFGHIJKLMNOPQRSTUVW"
 	local result, d = ""
 	while val > 0 do
-		val, d = math.floor(val/base), math.fmod(val,base)+1
+		val, d = math.floor(val/base), (val%base)+1
 		result = string.sub(k,d,d)..result
 	end
-	result = result:sub(1,8)
-	local todo = fill - #result
-	result = string.rep("0",todo)..result
+	if fill then
+		result = result:sub(1,fill)
+		local todo = fill - #result
+		result = string.rep("0",todo)..result
+	end
 	return result
 end
 

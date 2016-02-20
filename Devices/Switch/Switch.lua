40d74b47262b2e66a038c4f4ae4395acecb98cc4
Creator <wassil.janssen@gmail.com>
2016-02-20 13:07:26 +0200

    added stuff

--- Devices/Switch/Switch.lua
+++ Devices/Switch/Switch.lua
@@ -3,27 +3,35 @@
 ]]--
 
 local Delta = ...
-local base = "192.168."
 
-local function Switch(m)
-
-	m.open(65535)
-
-	while true do
-		event = {coroutine.yield("modem_message")}
-		if event[3] ==  and event[4] == 0x0 then
-			print("Answering request...")
-			m.transmit(1023,0x1,{
-				[1] = event[5],
-				[2] = base..tostring(moreSig).."."..tostring(lessSig)
-			})
-			lessSig = lessSig + 1
-			if lessSig == 256 then
-				moreSig = moreSig + 1
-				lessSig = 0
+local function wrap(side)
+	if peripheral.isPresent(side) then
+		if peripheral.getType(side) == "modem" then
+			if peripheral.call(side,"isWireless") == false then
+				print(side)
+				return Delta.modem(side)
 			end
 		end
 	end
+	return nil
 end
 
-return DHCP
+local function Switch()
+	local modems = {
+		top = wrap("top"),
+		bottom = wrap("bottom"),
+		front = wrap("front"),
+		back = wrap("back"),
+		right = wrap("right"),
+		left = wrap("left"),
+	}
+
+	
+	
+	while true do
+		event = {coroutine.yield("modem_message")}
+
+	end
+end
+
+return Switch

6e4dbf6d5b0f24b2edda21c55ad5e9ebea0855e1
Creator <wassil.janssen@gmail.com>
2016-02-26 20:08:57 +0200

    Built version 16w8a which adds a lot of stuf.

--- Devices/Gateway/Gateway.lua
+++ Devices/Gateway/Gateway.lua
@@ -1,13 +1,27 @@
 --[[
 	Gateway Service
+
+	IP_PACKET
+	{
+		[1] = Destination IP
+		[2] = Sender IP
+		[3] = Destination Port
+		[4] = Sender Port
+		[5] = Message
+		[6] = TTL
+	}
 ]]--
 
 local Delta = ...
 
 local function Gateway(globalInterface, globalSide, ipSide, modems, NAT)
+	local side, channel, protocol, IPpacket, destIP, sendIP, destPort, sendPort, message, TTL
 	while true do
 		event = {coroutine.yield("modem_message")}
-		--print(textutils.serialise(event))
+		side, channel, protocol, IPpacket = event[2], event[3], event[4], event[5]
+		if channel == 64511 and protocol == 0x0 and type(IPpacket) == "table" then
+			
+		end
 	end
 end
 

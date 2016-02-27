8143a7ede68542acd600e5c419f75dc85c9b1ebc
Creator <wassil.janssen@gmail.com>
2016-02-27 21:03:25 +0200

    Woring on FTP server/client

---
+++ Protocols/FTP/Server.lua
@@ -0,0 +1,35 @@
+--[[
+	Delta Networking FTP Server.
+]]--
+
+local path = ...
+
+dofile = function(path,...)
+	local f, err = loadfile(path)
+	if not f then
+		print(err)
+	end
+	setfenv(f,_G)
+	return f(...)
+end
+
+local Delta = dofile(path.."/init.lua", path)
+
+local permissions = {
+	list(string path) 	table fil
+	exists(string path) 	boolean e
+	isDir(string path) 	boolean i
+	isReadOnly
+	getName
+	getDrive
+	getSize
+	getFreeSpace
+	makeDir
+	move
+	copy
+	delete
+	combine
+	open
+	find 	
+	getDir 
+}
